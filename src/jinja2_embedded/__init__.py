90fd9b5df74a9451a096bacffefe24af09e8a1ae
GPla <36087062+GPla@users.noreply.github.com>
2024-05-12 16:23:51 +0200

    changed metadata

--- src/jinja2_embedded/__init__.py
+++ src/jinja2_embedded/__init__.py
@@ -7,7 +7,7 @@ __all__ = [
 __version__ = '0.1.0'
 
 __title__ = 'jinja2-embedded'
-__description__ = 'Jinja2 template loader for embedded Python runtime.'
+__description__ = 'Jinja2 template loader for embedded Python runtimes'
 
 __author__ = 'Gorden Platz'
 __email__ = '36087062+GPla@users.noreply.github.com'
